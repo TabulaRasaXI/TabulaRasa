80c4f6fb7c7d194eacb48c18508027c843d4be35
Nireya <aesh@protonmail.com>
2022-07-06 17:40:43 -0400

    Update battleentity.cpp

--- src/map/entities/battleentity.cpp
+++ src/map/entities/battleentity.cpp
@@ -86,7 +86,7 @@ CBattleEntity::CBattleEntity()
     m_Immunity   = 0;
     isCharmed    = false;
     m_unkillable = false;
-    m_dmgType = (DAMAGETYPE)0;
+    m_dmgType = (DAMAGE_TYPE)0;
 
     m_DeathType = DEATH_TYPE::NONE;
     BattleHistory.lastHitTaken_atkType = ATTACK_TYPE::NONE;
