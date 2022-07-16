7dd6f79bdbdeb0c2ff9a010e431a9abcc4c32387
Soapytoast <neosaidin@hotmail.com>
2022-07-16 14:51:30 -0400

    Update 75_era_vendors.lua
    Removing Repose, adding Aquaveil

--- modules/era/lua/75_era_vendors.lua
+++ modules/era/lua/75_era_vendors.lua
@@ -20,6 +20,26 @@ xi.customShop.Mjoll =
     5063,   1000, 3, -- Scroll of Ice Threnody
 }
 
+xi.customShop.Sororo =
+{
+    4641,  1165, 1, -- Diaga
+    4662,  7025, 1, -- Stoneskin
+    4664,   837, 1, -- Slow
+    4610,   585, 2, -- Cure II
+    4636,   140, 2, -- Banish
+    4646,  1165, 2, -- Banishga
+    4661,  2097, 2, -- Blink
+    4608,    61, 3, -- Cure
+    4615,  1363, 3, -- Curaga
+    4622,   180, 3, -- Poisona
+    4623,   324, 3, -- Paralyna
+    4624,   990, 3, -- Blindna
+    4606,    82, 3, -- Dia
+    4651,   219, 3, -- Protect
+    4656,  1584, 3, -- Shell
+    4663,   368, 3, -- Aquaveil
+}
+
 --Kazham
 xi.customShop.Toji_Mumosulah =
 {
@@ -288,6 +308,7 @@ local lookupTable =
 {
  -- Bastok Markets
     {"nation", "Bastok_Markets", "Mjoll", xi.customShop.Mjoll, xi.quest.fame_area.BASTOK, "MJOLL_SHOP_DIALOG", 1},
+	{"nation", "Bastok_Markets", "Sororo", xi.customShop.Sororo, xi.quest.fame_area.BASTOK, "SORORO_SHOP_DIALOG", 1},
  -- Kazham
     {"standard", "Kazham", "Toji_Mumosulah", xi.customShop.Toji_Mumosulah, xi.quest.fame_area.Kazham, "TOJIMUMOSULAH_SHOP_DIALOG", 1},
  -- Lower Jeuno
